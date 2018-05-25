037e003d9a2dcc59ec1cfd590c20031b0efcfb30
Andrew Mcveigh <me@andrewmcveigh.com>
2018-05-25 08:56:14 +0100

    Update layout

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -131,7 +131,7 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
-  (Key_Backtick, Key_1,         Key_2,     Key_3,      Key_4, Key_5, Key_LEDEffectNext,
+  (Key_Backtick, Key_1,         Key_2,     Key_3,      Key_4, Key_5, ___, // Key_LEDEffectNext,
    Key_Tab,      Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, Key_Tab,
    Key_Escape,    Key_A,         Key_O,     Key_E,      Key_U, Key_I,
    Key_LeftShift, Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_LeftAlt,
@@ -162,10 +162,10 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  ___,
+  (___,      Key_F1,           Key_F2,      Key_F3,        Key_F4,        Key_F5,           XXX,
+   Key_Tab,  ___,              Key_mouseUp, ___,           Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR,    Key_mouseBtnL, Key_mouseWarpNW,
+   Key_End,  Key_PrintScreen,  Key_Insert,  Key_DownArrow, Key_UpArrow,   Key_mouseWarpSW,  ___,
    ___, ___, Key_RightAlt, ___,
    ___,
 
